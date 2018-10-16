5b43d3ca998f969da66294cd4f4a2ca270405a5b
jvanwin <vanwi@DESKTOP-3VH0G19>
2018-10-16 14:28:58 -0400

    Spruced up documentation. Added checkBounds(index) call in next(..).

--- CSE 201 Group Project/src/Board.java
+++ CSE 201 Group Project/src/Board.java
@@ -1,21 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a mancala board represented as a list of {@code Bin} objects. It
+ * provides wrappers and convenience methods with features specific to mancala.
+ * 
+ * @author CSE 201 Team B - Fall 2018
+ * @version 0.9
+ */
 public class Board
 {
-	/*
-	 * Bin 6 and bin 13 are mancalas. Bins 0 through 6 belong to player one.
-	 * Bins 7 through 13 belong to player two.
-	 */
 	public final static int SIZE = 14;
 	
 	private List<Bin> bins;
 	
+	/**
+	 * Calls {@code this.initialize()}.
+	 */
 	public Board()
 	{
 		initialize();
 	}
 	
+	/**
+	 * <p>
+	 * Populates list with bins containing four stones, except mancalas, which
+	 * remain empty. For an existing {@code Board}, this returns it to a fresh
+	 * state.
+	 * </p>
+	 * 
+	 * <p>
+	 * The bins with the indices 0-6 are owned by {@code Player.ONE} and the
+	 * indices 7-13 by {@code Player.TWO}. The bins at indices 6 and 13 are
+	 * mancalas.
+	 * </p>
+	 */
 	public void initialize()
 	{
 		bins = new ArrayList<>(SIZE);
@@ -24,15 +43,23 @@ public class Board
 		for(int i = 0; i < SIZE; i++)
 		{
 			Player p = (i < midpoint) ? Player.ONE : Player.TWO;
-			boolean isMancala = (i % midpoint == midpoint - 1); // only indices
-																// 6 and 13
-			int stones = (isMancala) ? 0 : 4; // 0 for mancala, 4 for everything
-												// else
+			
+			// only true if 6 or 13
+			boolean isMancala = (i % midpoint == midpoint - 1);
+			
+			// 0 for mancala, 4 for all else
+			int stones = (isMancala) ? 0 : 4;
 			
 			bins.add(i, new Bin(stones, isMancala, p));
 		}
 	}
 	
+	/**
+	 * Return the number of stones at a given bin, specified by index number.
+	 * 
+	 * @param index Index of bin to check
+	 * @return number of stones at given index.
+	 */
 	public int stones(int index)
 	{
 		checkBounds(index);
@@ -40,6 +67,13 @@ public class Board
 		return bins.get(index).getStones();
 	}
 	
+	/**
+	 * Returns {@code true} if specified bin is a mancala and {@code false}
+	 * otherwise.
+	 * 
+	 * @param index Index of bin to check
+	 * @return whether bin is a mancala
+	 */
 	public boolean isMancala(int index)
 	{
 		checkBounds(index);
@@ -47,6 +81,13 @@ public class Board
 		return bins.get(index).isMancala();
 	}
 	
+	/**
+	 * Returns the player who owns the specified bin. This is either
+	 * {@code Player.ONE} or {@code Player.TWO}.
+	 * 
+	 * @param index Index of bin to check
+	 * @return owner of bin as member of {@code Player} enum.
+	 */
 	public Player player(int index)
 	{
 		checkBounds(index);
@@ -54,6 +95,12 @@ public class Board
 		return bins.get(index).player;
 	}
 	
+	/**
+	 * Returns a {@code Bin} from specified index.
+	 * 
+	 * @param index Index of bin to return
+	 * @return {@code Bin} object at a given index
+	 */
 	public Bin getBin(int index)
 	{
 		checkBounds(index);
@@ -61,6 +108,12 @@ public class Board
 		return bins.get(index);
 	}
 	
+	/**
+	 * Sets the number of stones a specified bin holds.
+	 * 
+	 * @param index Index of bin to modify
+	 * @param count Number of stones to set bin with
+	 */
 	public void setBinCount(int index, int count)
 	{
 		checkBounds(index);
@@ -75,7 +128,7 @@ public class Board
 	
 	/**
 	 * Returns the index of the bin on the opposite side of the one specified.
-	 * For the mancalas (indices 0 and 7), it returns the index of the other
+	 * For the mancalas (indices 6 and 13), it returns the index of the other
 	 * mancala.
 	 * 
 	 * @param index Index to find opposite of
@@ -89,7 +142,7 @@ public class Board
 		if(index == 0)
 		{
 			return SIZE / 2;
-		}else if(index == SIZE / 2)
+		} else if(index == SIZE / 2)
 		{
 			return 0;
 		}
@@ -97,11 +150,33 @@ public class Board
 		return SIZE - index; // TODO check logic
 	}
 	
+	/**
+	 * <p>
+	 * Returns the next bin's index from the one specified. The board is cyclic,
+	 * so an index of 13 returns 0.
+	 * </p>
+	 * <p>
+	 * This method return the number equivalent to {@code (index + 1) % 14}.
+	 * </p>
+	 * 
+	 * @param index Index from which the next index is found.
+	 * @return Index of next bin
+	 */
 	public static int next(int index)
 	{
+		checkBounds(index);
+		
 		return (index + 1) % SIZE;
 	}
 	
+	/**
+	 * Moves a stone from {@code source} to {@code destination}. The number of
+	 * stones is decremented for the source and incremented for the destination.
+	 * There are no checks for moving from zero or negative bins.
+	 * 
+	 * @param source      Bin to move a stone from.
+	 * @param destination Bin to move a stone to.
+	 */
 	public void move(int source, int destination)
 	{
 		checkBounds(source);
@@ -111,6 +186,12 @@ public class Board
 		bins.get(destination).increment();
 	}
 	
+	/**
+	 * Makes a move between bins as described by a {@code Move} object. There
+	 * are no checks for moving from zero or negative bins.
+	 * 
+	 * @param move
+	 */
 	public void move(Move move)
 	{
 		checkBounds(move.first());
@@ -120,6 +201,13 @@ public class Board
 		bins.get(move.second()).increment();
 	}
 	
+	/**
+	 * Used internally to throw an exception when an index lies outside of the
+	 * range 0-13 inclusive.
+	 * 
+	 * @param index Index to check
+	 * @throws IndexOutOfBoundsException on invalid index
+	 */
 	private static void checkBounds(int index) throws IndexOutOfBoundsException
 	{
 		if(0 > index || index >= SIZE)
