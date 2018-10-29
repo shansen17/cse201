0e99b6e724731d1423298568825b1ed9cb855dde
jvanwin <43450228+jvanwin@users.noreply.github.com>
2018-10-29 13:56:56 -0400

    gameWon() (#9)
    * other test
    
    * Create README.md
    
    * Update README.md
    
    * gamewon

--- CSE 201 Group Project/src/GameState.java
+++ CSE 201 Group Project/src/GameState.java
@@ -111,10 +111,30 @@ public class GameState
 		return true;
 	}
 	
-	// TODO finish
 	public boolean gameWon()
 	{
-		return false;
+		boolean empty1 = true;
+		boolean empty2 = true;
+		
+		for (int i = 0; i < 6; i++)
+		{
+			if (board.stones(i) != 0)
+			{
+				empty1 = false;
+				break;
+			}
+		}
+		
+		for (int i = 7; i < 12; i++)
+		{
+			if (board.stones(i) != 0)
+			{
+				empty2 = false;
+				break;
+			}
+		}
+		
+		return empty1 || empty2;
 	}
 	
 	public boolean capturePossible(int lastMoveDest)
@@ -126,7 +146,7 @@ public class GameState
 		{
 			return false;
 		}
-		// only capture with stone placed in empty bin
+		// do not capture when not empty bin
 		if(board.stones(lastMoveDest) != 1)
 		{
 			return false;
---
+++ README.md
@@ -0,0 +1,3 @@
+# CSE201 Group Project Team B
+
+We won't accept pull requests from outsiders, but feel free to open bug reports. 😉
