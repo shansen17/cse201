8ab54a97c538d8ed0e594acb35c17f52e92f944a
jvanwin <vanwi@DESKTOP-3VH0G19>
2018-11-04 14:06:37 -0500

    Added game loop. Needs debugging.

--- CSE 201 Group Project/src/MancalaMain.java
+++ CSE 201 Group Project/src/MancalaMain.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class MancalaMain
 {
 	public static void main(String[] args)
@@ -21,35 +19,13 @@ public class MancalaMain
 	public static boolean playGame()
 	{
 		GameState game = new GameState();
-		PlayerInput human = new HumanInput();
-		PlayerInput machine = new MachineInput();
+		//PlayerInput human = new HumanInput();
+		//PlayerInput machine = new MachineInput();
 		
 		while(!game.gameWon())
 		{
-			if(game.whoseTurn == Player.ONE)
-				System.out.println(game);
 			
-			int nextMove;
-			PlayerInput input;
-			if(game.whoseTurn == Player.ONE)
-			{
-				input = human;
-			} else
-			{
-				input = machine;
-			}
-			
-			nextMove = input.getMove(game.getBoard());
-			
-			if(!game.validMove(nextMove))
-			{
-				continue;
-			}
-			
-			List<Move> moveSet = game.move(nextMove);
 		}
-		
-		System.out.println(game.whoseTurn + " won!");
 		
 		return true;
 	}
